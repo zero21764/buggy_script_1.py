2951a9941ce92a825018363fdf5ebc9c61fb6216
zero21764 <78752053+zero21764@users.noreply.github.com>
2025-09-07 18:13:56 -0700

    Add files via upload

---
+++ buggy_script_1.py
@@ -0,0 +1,4 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # buggy_script_1.py (intentionally broken)
+... print "Hello"
